966f445234d2267b3eb70659a439acec9f335871
a.vasilev-dc <a.vasilev@delivery-club.ru>
2017-08-08 14:28:24 +0300

    fix pull from repo

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,15 +1,15 @@
 # Load settings
+echo "Updating configuration"
+(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+
 source ~/dotfiles/zsh/zshrc.sh
 
 # Run tmux if exists
 if command -v tmux>/dev/null; then
-	[ -z $TMUX ] && exec tmux
+    [ -z $TMUX ] && exec tmux
 else
-	echo "tmux not installed. Run ./deploy to configure dependencies"
+    echo "tmux not installed. Run ./deploy to configure dependencies"
 fi
-
-echo "Updating configuration"
-(cd ~/dotfiles && git pull && git submodule update --init --recursive)
 
 # time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 # (cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
